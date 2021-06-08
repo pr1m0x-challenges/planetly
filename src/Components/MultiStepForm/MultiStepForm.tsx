ddfd8cafd072a9d2e1e983d7d49dc0019931f435
pr1m0x <kraemer.timo1@gmail.com>
2021-06-08 17:52:29 +0200

    created next and back handler func and step indicator arr

--- src/Components/MultiStepForm/MultiStepForm.tsx
+++ src/Components/MultiStepForm/MultiStepForm.tsx
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+import { Grid } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const steps = ['Day 1', 'Day 2', 'Day 3'];
 
 export default function MultiStepForm() {
   const [activeStep, setActiveStep] = useState<number>(0);
@@ -6,5 +10,21 @@ export default function MultiStepForm() {
   const [errors, setErrors] = useState<object>({});
   const [emissionData, setEmissionData] = useState<object[]>();
 
-  return <main></main>;
+  const handleNext = () => {
+    setActiveStep(activeStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
+  };
+
+  return (
+    <>
+      <CssBaseline />
+
+      <main>
+        <Grid container direction="row" spacing={6} style={{ justifyContent: 'center' }}></Grid>
+      </main>
+    </>
+  );
 }
