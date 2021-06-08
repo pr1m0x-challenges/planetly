cfd9ebb698baba1862decbaa778ee2c92b2f166b
pr1m0x <kraemer.timo1@gmail.com>
2021-06-08 22:34:05 +0200

    added validation schema with yup and validate page data on handleNext click

--- src/Components/MultiStepForm/MultiStepForm.tsx
+++ src/Components/MultiStepForm/MultiStepForm.tsx
@@ -3,6 +3,8 @@ import { Grid, Paper, Stepper, Step, StepLabel, Button } from '@material-ui/core
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { FormStyles } from '../../styles/FormStyles';
 import { FormStepPage } from './FormStepPage';
+import { IFormData, IFormDataIndex } from '../../interfaces/interfaces';
+import { FormValidation } from '../../services/validations/MultiStepFormValidations';
 
 const useStyles = FormStyles();
 const steps = ['Day 1', 'Day 2', 'Day 3'];
@@ -15,11 +17,18 @@ export default function MultiStepForm() {
   const classes = useStyles();
 
   const [activeStep, setActiveStep] = useState<number>(0);
-  const [formData, setFormData] = useState<object>({});
+  const [formData, setFormData] = useState<IFormDataIndex>({});
   const [errors, setErrors] = useState<object>({});
   const [emissionData, setEmissionData] = useState<object[]>();
 
-  const handleNext = () => {
+  const handleNext = async () => {
+    const currentFormPageData: IFormData = formData[activeStep];
+
+    const formErrors = await FormValidation(currentFormPageData);
+
+    if (formErrors) {
+      return;
+    }
     setActiveStep(activeStep + 1);
   };
 
---
+++ src/services/validations/MultiStepFormValidations.ts
@@ -0,0 +1,17 @@
+import * as yup from 'yup';
+import { IFormData } from '../../interfaces/interfaces';
+
+let schema = yup.object().shape({
+  mwh: yup.string().required('Please enter your mwh'),
+  location: yup.string().required('Please enter a location'),
+  date: yup.string().required('Please enter a date'),
+});
+
+export const MultiStepFormValidations = (v: IFormData): false | object => {
+  try {
+    schema.validateSync(v, { abortEarly: false });
+    return false;
+  } catch (err) {
+    return err.inner;
+  }
+};
