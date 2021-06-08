646d50efaed7068e6109039bd93f089afe40c637
pr1m0x <kraemer.timo1@gmail.com>
2021-06-08 21:42:38 +0200

    created Datepicker Component

---
+++ src/Components/DatePicker.tsx
@@ -0,0 +1,23 @@
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import { IProps } from '../Interfaces/interfaces';
+
+const DatePicker = ({ formData, pageStep, handleChange }: IProps) => {
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <KeyboardDatePicker
+        disableToolbar
+        variant="inline"
+        format="dd.MM.yyyy"
+        margin="normal"
+        id="date-picker-inline"
+        value={formData[pageStep]?.date || null}
+        onChange={(picktedDate: Date) => {
+          handleChange(picktedDate, 'date');
+        }}
+      />
+    </MuiPickersUtilsProvider>
+  );
+};
+
+export default DatePicker;
