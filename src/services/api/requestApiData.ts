eb4da309e8d989a52e2f6a8be456883756c841c0
pr1m0x <kraemer.timo1@gmail.com>
2021-06-08 16:46:58 +0200

    connected to api with static values

--- src/services/api/requestApiData.ts
+++ src/services/api/requestApiData.ts
@@ -30,16 +30,28 @@ interface CarbonResponseAttributes {
 }
 
 export const requestData: Function = async (formData: any): Promise<any> => {
+  const token = process.env.REACT_APP_API_KEY;
+  const url = '!!https://www.carboninterface.com/api/v1/estimates!!';
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+  const bodyParameters = {
+    type: 'electricity',
+    electricity_unit: 'mwh',
+    electricity_value: '18',
+    country: 'us',
+  };
+
   try {
     return new Promise(async (resolve, reject) => {
       // const keys = Object.keys(formData);
-      const keys = [1, 2, 3];
 
+      const keys = [1];
       const allData = Promise.all(
         keys.map((key) => {
           return new Promise(async (resolve, reject) => {
             try {
-              const res = await axios.get('https://randomuser.me/api/');
+              const res = await axios.post(url, bodyParameters, config);
               resolve(res);
             } catch (err) {
               console.log(err);
